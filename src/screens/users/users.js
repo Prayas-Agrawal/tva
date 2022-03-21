57ad6caa29f49a35312492e0a105e0968e8e039c
Prayas Agrawal <pagrawal@innominds.com>
2022-03-21 15:29:10 +0530

    Making data grid

--- src/screens/users/users.js
+++ src/screens/users/users.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { Api } from "../../api/api";
 import { UserDataRow } from "../../models/userModel";
+import DataGrid from "../../components/dataGrid/dataGrid";
+import Search from "../../components/search";
+import { Utils } from "../../utils/utils";
+import "./users.css";
 
 export default function Users() {
   const [userRows, setUserRows] = useState([]);
@@ -10,10 +14,30 @@ export default function Users() {
       const _usersList = await Api.getUsers();
       const _userRows = _usersList.map((_json) => new UserDataRow(_json));
       setUserRows(_userRows);
+      console.log("got data: ", _userRows);
     } catch (e) {
       console.log("somethings up: ", e);
     }
   }, []);
 
-  return <>{userRows.map((val) => <div>{val.first_name}</div>)}</>;
+  function handleSort(key, order) {
+    const sortedData = Utils.sort(userRows, key, order);
+    setUserRows((val) => {
+      return sortedData;
+    });
+  }
+
+  return (
+    <div className="users-container-style">
+      {/* <Search /> */}
+      <div className="users-header">Users</div>
+      <DataGrid
+        data={userRows}
+        pageSize={10}
+        sortCallback={handleSort}
+        noDataText="Sorry! No Data present in table"
+        noSearchResultsText="Search returned 0 rows. Please try searching something different"
+      />
+    </div>
+  );
 }
